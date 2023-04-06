28b71f13f9f50511608b797d84a4a7ac7f43ccc2
hehehe0803 <hehehe0803@gmail.com>
2023-04-06 19:44:19 +0200

    edit main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 if __name__ == '__main__':
     import time
-    import flappy_bird_gym
+    import flappy_bird_gymnasium
+    import gymnasium
 
-    env = flappy_bird_gym.make("FlappyBird-v0")
+    env = gymnasium.make("FlappyBird-v0")
 
-    obs = env.reset()
-    action = env.action_space.sample()
-    print(action)
-    obs, reward, done, info = env.step(action)
-    print(obs, reward, done, info)
-    # while True:
-    #     # Next action:
-    #     # (feed the observation to your agent here)
-    #     action = env.action_space.sample()
-    #
-    #     # Processing:
-    #     obs, reward, done, info = env.step(action)
-    #
-    #     # Rendering the game:
-    #     # (remove this two lines during training)
-    #     env.render()
-    #     time.sleep(1 / 30)  # FPS
-    #
-    #     # Checking if the player is still alive
-    #     if done:
-    #         break
+    obs, _ = env.reset()
+    while True:
+        # Next action:
+        # (feed the observation to your agent here)
+        action = env.action_space.sample()
+
+        # Processing:
+        obs, reward, terminated, _, info = env.step(action)
+        print(obs)
+        print(info)
+        # Rendering the game:
+        # (remove this two lines during training)
+        env.render()
+        time.sleep(1 / 30)  # FPS
+
+        # Checking if the player is still alive
+        if terminated:
+            break
 
     env.close()
