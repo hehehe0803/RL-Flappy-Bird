bf3ec75966a44eb62c31ae63042dc6cafb2ca8c6
Hoang V. Pham <hoangv.pham0803@gmail.com>
2023-05-02 11:47:39 +0100

    add play get scores

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
+import glob
 import json
+import re
 import time
 
 import flappy_bird_gym
 import matplotlib.pyplot as plt
+import numpy as np
 import pygame
 
 
@@ -53,3 +56,72 @@ def play():
             break
 
     env.close()
+
+
+def combine_history(pattern: str) -> list:
+    filenames = sorted(glob.glob(pattern), key=lambda name: int(re.search(r"\d+", name).group(0)))
+
+    print(filenames)
+
+    combined_scores = []
+    for filename in filenames:
+        with open(filename, "r") as file:
+            scores = json.load(file)
+            combined_scores.extend(scores)
+
+    return combined_scores
+
+def plot_combined_scores(combined_scores: list):
+    episodes = np.arange(1, len(combined_scores) + 1)
+    mean_scores = np.cumsum(combined_scores) / episodes
+    max_scores = np.maximum.accumulate(combined_scores)
+
+    plt.figure(figsize=(12, 6))
+
+    plt.scatter(episodes, combined_scores, label='Score', alpha=0.5, s=5)
+    plt.plot(episodes, mean_scores, label='Mean Score', color='red')
+    plt.plot(episodes, max_scores, label='Max Score', color='green')
+
+    plt.xlabel('Episode')
+    plt.ylabel('Score')
+    plt.legend()
+    plt.title('Scores per Episode')
+    plt.grid()
+
+    plt.gca().set_yscale('log')
+
+    plt.show()
+
+def play(n_episodes: int):
+    env = flappy_bird_gym.make("FlappyBird-v0")
+    scores = list()
+
+    for e in range(n_episodes):
+        clock = pygame.time.Clock()
+
+        obs = env.reset()
+        while not done:
+            env.render()
+
+            # Getting action:
+            action = 0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN and (
+                    event.key == pygame.K_SPACE or event.key == pygame.K_UP
+                ):
+                    action = 1
+
+            # Processing:
+            obs, reward, done, info = env.step(action)
+
+            clock.tick(15)
+
+            if done:
+                env.render()
+                scores.append(info["score"])
+                time.sleep(1)
+
+    env.close()
+    return scores
