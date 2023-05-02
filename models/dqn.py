58e84abb3dc12f6d8120d8e1c5f1aa25be434318
Hoang V. Pham <hoangv.pham0803@gmail.com>
2023-05-02 11:49:14 +0100

    add rgb and features neural network models

--- models/dqn.py
+++ models/dqn.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 import time
+import shutil
 from random import random
 from typing import Dict, NamedTuple
 
@@ -9,13 +10,13 @@ import gym
 import keras
 import numpy as np
 from keras import Input, Model
-from keras.initializers.initializers_v2 import HeUniform
-from keras.layers import Conv2D, Dense, Flatten
+from keras.initializers import HeUniform
+from keras.layers import Dense
 from keras.optimizers import Adam
 import tensorflow as tf
 
 RANDOM_SEED = 42
-rng = np.random.RandomState(42)
+rng = np.random.RandomState(RANDOM_SEED)
 
 MODEL_WEIGHTS_PATH = "model_weights/"
 HISTORY_PATH = "history/"
@@ -29,16 +30,16 @@ class Transition(NamedTuple):
     next_state: np.ndarray
     terminal: bool
 
-class RGBReplay:
+class Replay:
     def __init__(self, cfg: Dict):
         self.buffer_size = int(cfg["buffer_size"])
         self.index = 0
         self.full = False
 
-        self.states = np.zeros((self.buffer_size, 144, 256, 1))
+        self.states = np.zeros((self.buffer_size, 2))
         self.actions = np.zeros(self.buffer_size, dtype=np.uint8)
         self.rewards = np.zeros(self.buffer_size, dtype=np.uint8)
-        self.next_states = np.zeros((self.buffer_size, 144, 256, 1))
+        self.next_states = np.zeros((self.buffer_size, 2))
         self.terminal = np.zeros(self.buffer_size, dtype=np.bool_)
         self.priorities = np.zeros(self.buffer_size)
 
@@ -96,24 +97,23 @@ class RGBReplay:
             pickle.dump(self, f)
 
 
-class RGBDQN(object):
+class DQN(object):
     def __init__(self, cfg):
-        self.cfg = cfg
-
+        self.reward_scaling = cfg["reward_scaling"]
         self.update_frequency = cfg["experience_update_frequency"]
-        self.experience = RGBReplay(cfg)
+        self.experience = Replay(cfg)
 
         self.gamma = cfg["gamma"]
 
         self.epsilon = cfg["epsilon"]["start"]
-        self.epsilon_end = cfg["epsilon"]["end"]
         self.epsilon_step = int(cfg["epsilon"]["step"])
-        self.delta_epsilon = (self.epsilon_end - self.epsilon) / self.epsilon_step
+        self.epsilon_end = cfg["epsilon"]["end"]
+        self.inc = (self.epsilon_end - self.epsilon) / self.epsilon_step
         self.bound = max
 
         self.batch_size = int(cfg["batch_size"])
 
-        self.frame_dim = (144, 256, 1)
+        self.feature_dim = (2,)
         self.action_space = [0, 1]
 
         self.step_count = 0
@@ -128,78 +128,57 @@ class RGBDQN(object):
     def _make_model(self, cfg: Dict[str, int]) -> keras.Model:
         kernel_init = HeUniform(seed=int(cfg["random_seed"]))
 
-        inputs = Input(shape=self.frame_dim)
-        conv1 = Conv2D(
-            32,
-            kernel_size=8,
-            strides=(4, 4),
-            padding="valid",
-            activation='relu',
-            kernel_initializer=kernel_init,
-        )(inputs)
-        conv2 = Conv2D(
-            64,
-            kernel_size=4,
-            strides=(2, 2),
-            padding="valid",
-            activation='relu',
-            kernel_initializer=kernel_init,
-        )(conv1)
-        conv3 = Conv2D(
-            64,
-            kernel_size=3,
-            strides=(1, 1),
-            padding="valid",
-            activation='relu',
-            kernel_initializer=kernel_init,
-        )(conv2)
-        flat = Flatten()(conv3)
-
-        fc1 = Dense(512, activation='relu', kernel_initializer=kernel_init)(flat)
-        outputs = Dense(2, kernel_initializer=kernel_init)(fc1)
+        inputs = Input(shape=self.feature_dim)
+        x = Dense(32, activation='relu', kernel_initializer=kernel_init)(inputs)
+        x = Dense(32, activation='relu', kernel_initializer=kernel_init)(x)
+        outputs = Dense(2, activation='linear', kernel_initializer=kernel_init)(x)
 
         model = Model(inputs=inputs, outputs=outputs)
         model.compile(loss="mse", optimizer=Adam(learning_rate=cfg["learning_rate"]))
 
         return model
 
-    def process_image(self, state: np.ndarray) -> np.ndarray:
-        resized_state = cv2.resize(state, (256, 144))
-        gray_state = cv2.cvtColor(resized_state, cv2.COLOR_BGR2GRAY)
-        normalized_state = gray_state / 255.0
-        img_expanded = normalized_state[None ,:, :, None]
-        return img_expanded
-
     def choose_action(self, state) -> int:
         if np.random.rand() < self.epsilon:
-            action = np.random.choice(2, p=[0.95, 0.05])
+            action = np.random.choice(2, p=[0.9,0.1])
         else:
-            q_values = self.model(tf.convert_to_tensor(state)).numpy()
+            q_values = self.model(np.reshape(state, [1, 2])).numpy()
             action = np.argmax(q_values)
         return action
 
 
+    def custom_reward(self, state, reward):
+        x_pos = state[0]
+        y_dif = state[1]
+
+        x_pos_scaling = 1 / (1 + x_pos)
+
+        reward -= self.reward_scaling * x_pos_scaling * abs(y_dif)
+        reward = np.round(reward, 4)
+
+        return reward
 
     def learn(self):
         states, actions, rewards, next_states, terminals, indices = self.experience.sample(self.batch_size)
 
-        q_values = self.model(tf.convert_to_tensor(states))
-        q_target_next_values = self.target_model(next_states)
+        q_values = self.model(states).numpy()
+        q_target_next_values = self.target_model(next_states).numpy()
 
-        updates = rewards + self.gamma * np.max(q_target_next_values, axis=1) * ~terminals
+        best_action = np.argmax(q_values, axis=1)
+        updates = rewards + self.gamma * q_target_next_values[np.arange(self.batch_size), best_action] * ~terminals
 
-        q_values_target = q_values.numpy()
+        q_values_target = q_values
         q_values_target[np.arange(self.batch_size), actions] = updates
 
         td_error = np.abs(updates - q_values_target[np.arange(self.batch_size), actions])
         self.experience.update_priorities(td_error, indices)
 
-        loss = self.model.train_on_batch(states, q_values_target)
+        history = self.model.fit(states, q_values_target, batch_size=self.batch_size, verbose=False)
 
         if self.epsilon > self.epsilon_end:
-            self.epsilon = self.bound(self.epsilon + self.delta_epsilon, self.epsilon_end)
+            self.epsilon = self.bound(self.epsilon + self.inc, self.epsilon_end)
 
-        return loss
+        return history.history["loss"][0]
 
     def update_target_network(self):
         if self.step_count % self.network_update_frequency == 0:
@@ -210,7 +189,6 @@ class RGBDQN(object):
 
         for episode in range(num_episodes):
             state = env.reset()
-            state = self.process_image(state)
 
             terminal = False
             self.step_count = 1
@@ -220,7 +198,7 @@ class RGBDQN(object):
             while not terminal:
                 action = self.choose_action(state)
                 next_state, reward, terminal, info = env.step(action)
-                next_state = self.process_image(next_state)
+                reward = self.custom_reward(next_state, reward)
 
                 self.step_count += 1
                 self.experience.append(Transition(state, action, reward, next_state, terminal))
@@ -240,12 +218,14 @@ class RGBDQN(object):
             if (episode + 1) % save_interval == 0:
                 self.save_model(self.experiment_name + f"{episode + 1:05d}.h5")
                 self.save_score(self.experiment_name + f"{episode + 1:05d}.json")
+                self.experience.save_experience(self.experiment_name + "experience.plk")
+                self.save_epsilon(self.experiment_name + "epsilon.json")
 
             print(
                 f"Episode: {episode + 1:<4} "
-                f"Total Rewards: {training_rewards:<5} "
+                f"Total Rewards: {training_rewards:9.4f} "
                 f"Score: {info['score']:<4} "
-                f"Loss: {episode_loss / self.step_count:7.2f} "
+                f"Loss: {episode_loss / self.step_count:7.4f} "
                 f"Epsilon: {self.epsilon:.4f} "
                 f"Time Elapsed: {time.time() - start_time:.2f} s"
             )
@@ -253,28 +233,23 @@ class RGBDQN(object):
         return self.score
 
     def save_model(self, model_name: str = 'flap_dqn.h5'):
-        self.model.save(MODEL_WEIGHTS_PATH + model_name)
+        self.model.save("temp.h5")
+        shutil.move("temp.h5", MODEL_WEIGHTS_PATH + model_name)
+
+    def load_model(self, model_name: str):
+        self.model.load_weights(model_name)
+        self.target_model.load_weights(model_name)
 
     def save_score(self, name:str):
         with open(HISTORY_PATH + name, "w") as f:
             json.dump(self.score, f)
         self.score.clear()
 
-    def load_model(self, model_name: str):
-        self.model.load_weights(model_name)
-        self.target_model.load_weights(model_name)
-
-    def play(self, env):
-        state = env.reset()
-        state = self.process_image(state)
-
-        terminated = False
-        while not terminated:
-            q_values = self.model(state).numpy()
-            action = np.argmax(q_values)
-            state, _, terminated, info = env.step(action)
-            state = self.process_image(state)
-
-            # Rendering the game
-            env.render()
-            time.sleep(1 / 60)  # FPS
+    def save_epsilon(self, name: str):
+        with open("model_init/" + name, "w") as f:
+            json.dump(
+                {
+                    "epsilon": self.epsilon
+                },
+                f
+            )
